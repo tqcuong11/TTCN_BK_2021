e5a21d66c93139c1de8fb2db526b7cc3d54a47b8
tqcuong11 <cuongtran11052000@gmail.com>
2022-01-05 21:21:24 +0700

    Route detail product

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,6 @@
 const siteRouter = require("./siteRoutes");
 
 function route(app) {
-<<<<<<< HEAD
-  
-=======
->>>>>>> d75e6998c3a5ecb95b2a087135dc20e522691e78
   app.use("/", siteRouter);
 }
 
--- app/routes/siteRoutes.js
+++ app/routes/siteRoutes.js
@@ -3,14 +3,8 @@ const router = express.Router();
 
 const siteController = require("../controllers/SiteController");
 
-<<<<<<< HEAD
 router.get("/:slug",siteController.detail)
 router.get("/", siteController.home)
 
 
 module.exports = router
-=======
-router.get("/", siteController.home);
-
-module.exports = router;
->>>>>>> d75e6998c3a5ecb95b2a087135dc20e522691e78
