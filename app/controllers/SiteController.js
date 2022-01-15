dee5430989752af15bd237f7b35109b4b4d1e747
mquan247 <quannngo247@gmail.com>
2022-01-15 21:42:17 +0700

    admin

--- app/controllers/SiteController.js
+++ app/controllers/SiteController.js
@@ -1,5 +1,5 @@
-const Product= require('../models/Product');
-const User = require('../models/User');
+const Product = require("../models/Product");
+const User = require("../models/User");
 
 const SiteController = {
   // [GET] / home
@@ -8,46 +8,76 @@ const SiteController = {
       const products = await Product.find({});
       if (req.user) {
         const user = await User.findOne({ _id: req.user });
-        res.render('home', { products, user });
+        res.render("home", { products, user });
       } else {
-        res.render('home', { products, user: '' });
+        res.render("home", { products, user: "" });
       }
     } catch (err) {
-      return res.render('error', {
+      return res.render("error", {
         err,
-        message: 'Xảy ra lỗi khi nhận dữ liệu từ server, xin thử lại',
+        message: "Xảy ra lỗi khi nhận dữ liệu từ server, xin thử lại",
       });
     }
   },
+
+  // [GET] / detail
+  detail: async (req, res, next) => {
+    const product_slug = req.params.slug;
+    const product = await Product.findOne({ slug: product_slug }).exec();
+    if (req.user) {
+      const user = await User.findOne({ _id: req.user });
+      if (product) res.render("product-detail", { product, user });
+      else next();
+    } else {
+      if (product) res.render("product-detail", { product, user: "" });
+      else next();
+    }
+  },
+
   // [GET] / login
   login: (req, res) => {
-    res.render('login');
+    res.render("login");
   },
   // [GET] / register
   register: (req, res) => {
-    res.render('register');
+    res.render("register");
   },
-  
+
   // [POST] / info/logout
   logout: async (req, res) => {
-    res.clearCookie('access_token');
-    res.clearCookie('refresh_token');
+    res.clearCookie("access_token");
+    res.clearCookie("refresh_token");
     const refreshToken = req.cookies.refresh_token;
-    await User.updateOne({ refreshToken }, { refreshToken: '' });
+    await User.updateOne({ refreshToken }, { refreshToken: "" });
     try {
       const products = await Product.find({});
-      res.cookie('isLoggedIn', 'false');
-      res.render('home', {
+      res.cookie("isLoggedIn", "false");
+      res.render("home", {
         products: products,
-        user: '',
+        user: "",
       });
     } catch (err) {
-      res.render('error', {
+      res.render("error", {
         err,
-        message: 'Có lỗi khi nhận dữ liệu từ server, xin thử lại',
+        message: "Có lỗi khi nhận dữ liệu từ server, xin thử lại",
       });
     }
-  }
- 
+  },
+
+  async search(req, res) {
+    const search = req.query.search;
+    const products = await Product.find({
+      name: {
+        $regex: `.*${req.query.search}.*`,
+        $options: "$i",
+      },
+    });
+    if (req.user) {
+      const user = await User.findOne({ _id: req.user });
+      res.render("search", { products, user });
+    } else {
+      res.render("search", { products, user: "" });
+    }
+  },
 };
 module.exports = SiteController;
