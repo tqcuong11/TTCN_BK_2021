bbd109d234ad92b9d26315f07ef00101737fc8ac
mquan247 <quannngo247@gmail.com>
2022-01-17 10:41:33 +0700

    add cart

--- app/controllers/CartController.js
+++ app/controllers/CartController.js
@@ -1,75 +1,54 @@
-const User=require("../models/User");
-const Product=require('../models/Product');
+const User = require("../models/User");
+const Product = require("../models/Product");
 
-
-const CartController={
-    index : async (req,res)=>{
-        try {
-            if (req.user){
-                const user= await User.findOne({_id:req.user});
-                let products=[];
-                if (user.cart.length!==0){
-                    for (let index=0;index<user.cart.length;index++)
-                        products.push(await Product.findOne({_id : user.cart[index]}));
-                }
-                user.cart=products;
-                res.render("cart",{user});
-            }
-            else {
-                const user={};               
-                let cart=req.cookies.cart;
-                console.log(cart)
-                let products=[];
-                if (cart){
-                    cart=JSON.parse(cart);
-                    for (let index=0;index<cart.length;index++)
-                        products.push(await Product.findOne({_id : cart[index]}));
-                }
-                user.cart=products;
-                console.log(user)
-                res.render('cart',{user}); 
-
-            }
-        } catch (error) {
-            
+const CartController = {
+  index: async (req, res) => {
+    try {
+      if (req.user) {
+        const user = await User.findOne({ _id: req.user });
+        let products = [];
+        if (user.cart.length !== 0) {
+          for (let index = 0; index < user.cart.length; index++) products.push(await Product.findOne({ _id: user.cart[index] }));
         }
-    },
-    addProduct: async (req,res)=>{
-        try {
-            const productId=req.params.id;
-            if (req.user){
-                await User.updateOne(
-                    {_id : req.user},
-                    {$addToSet : {cart:[productId]} }
-                    );
-                res.redirect('back');
-            } else {
-                res.redirect('back');
-            }
-            
-        } catch (error) {
-            
+        user.cart = products;
+        res.render("cart", { user });
+      } else {
+        const user = {};
+        let cart = req.cookies.cart;
+        console.log(cart);
+        let products = [];
+        if (cart) {
+          cart = JSON.parse(cart);
+          for (let index = 0; index < cart.length; index++) products.push(await Product.findOne({ _id: cart[index] }));
         }
-    },
-    delete: async (req,res)=>{
-        try {
-            console.log(req.session.userId)
-            const productId=req.params.id;
-            if (req.user){
-                await User.updateOne(
-                    {_id : req.user},
-                    {$pull : {cart:{$in:[productId]} }}
-                    );
-                res.redirect('back');
-            } else {
-                res.redirect('back');
-            }
-            
-        } catch (error) {
-            
-        }
-    }
+        user.cart = products;
+        // console.log(user);
+        res.render("cart", { user });
+      }
+    } catch (error) {}
+  },
+  addProduct: async (req, res) => {
+    try {
+      const productId = req.params.id;
+      if (req.user) {
+        await User.updateOne({ _id: req.user }, { $addToSet: { cart: [productId] } });
+        res.redirect("back");
+      } else {
+        res.redirect("back");
+      }
+    } catch (error) {}
+  },
+  delete: async (req, res) => {
+    try {
+      const productId = req.params.id;
+      if (req.user) {
+        await User.updateOne({ _id: req.user }, { $pull: { cart: { $in: [productId] } } });
+        res.redirect("back");
+      } else {
+        res.redirect("back");
+      }
+    } catch (error) {}
+  },
+};
 
-}
-
-module.exports=CartController;
+module.exports = CartController;
--- app/routes/cartRoutes.js
+++ app/routes/cartRoutes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const CartController = require("../controllers/CartController");
-const cartController=require('../controllers/CartController');
-const verifyToken=require("../middlewares/verifyToken");
+const cartController = require("../controllers/CartController");
+const verifyToken = require("../middlewares/verifyToken");
 
-router.get("/delete/:id",verifyToken,cartController.delete);
-router.get("/add/:id",verifyToken,CartController.addProduct);
-router.get("/",verifyToken,cartController.index);
+router.get("/delete/:id", verifyToken, cartController.delete);
+router.get("/add/:id", verifyToken, CartController.addProduct);
+router.get("/", verifyToken, cartController.index);
 
-module.exports=router
+module.exports = router;
