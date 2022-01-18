69f7d0fdf91adf34053976ade3f972563e1b91ef
mquan247 <quannngo247@gmail.com>
2022-01-18 10:48:29 +0700

    admin

--- app/controllers/AdminController.js
+++ app/controllers/AdminController.js
@@ -21,9 +21,59 @@ const AdminController = {
   //   },
 
   // [GET] / login
-  dashboard: (req, res) => {
-    // res.render("admin", { message: "" });
-    res.render("admin");
+  dashboard: async (req, res) => {
+    try {
+      if (req.user) {
+        const user = await User.findOne({ _id: req.user });
+        res.render("admin", { user });
+      } else {
+        res.render("admin", { user: "" });
+      }
+    } catch (error) {}
+  },
+
+  manageProducts: async (req, res) => {
+    try {
+      if (req.user) {
+        const user = await User.findOne({ _id: req.user });
+        res.render("sanpham", { user });
+      } else {
+        res.render("sanpham", { user: "" });
+      }
+    } catch (error) {}
+  },
+
+  manageStaffs: async (req, res) => {
+    try {
+      if (req.user) {
+        const user = await User.findOne({ _id: req.user });
+        res.render("nhanvien", { user });
+      } else {
+        res.render("nhanvien", { user: "" });
+      }
+    } catch (error) {}
+  },
+
+  manageCustomers: async (req, res) => {
+    try {
+      if (req.user) {
+        const user = await User.findOne({ _id: req.user });
+        res.render("khachhang", { user });
+      } else {
+        res.render("khachhang", { user: "" });
+      }
+    } catch (error) {}
+  },
+
+  manageBills: async (req, res) => {
+    try {
+      if (req.user) {
+        const user = await User.findOne({ _id: req.user });
+        res.render("hoadon", { user });
+      } else {
+        res.render("hoadon", { user: "" });
+      }
+    } catch (error) {}
   },
 };
 module.exports = AdminController;
--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const AdminController = require("../controllers/AdminController");
+const verifyToken = require("../middlewares/verifyToken");
 
-router.get("/", AdminController.dashboard);
+router.get("/nhanvien", verifyToken, AdminController.manageStaffs);
+router.get("/sanpham", verifyToken, AdminController.manageProducts);
+router.get("/khachhang", verifyToken, AdminController.manageCustomers);
+router.get("/hoadon", AdminController.manageBills);
+router.get("/", verifyToken, AdminController.dashboard);
 
 module.exports = router;
