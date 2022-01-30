e587f7d6ac51857d2d684e2d460203b8f26d9ec5
tqcuong11 <cuongtran11052000@gmail.com>
2022-01-31 01:48:01 +0700

    Build checkRole and fix route

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -4,14 +4,14 @@ const AdminController = require("../controllers/AdminController");
 const auth = require("../controllers/Auth");
 const verifyToken = require("../middlewares/verifyToken");
 
-router.get("/employees", verifyToken, AdminController.listEmployees);
-router.get("/products", verifyToken, AdminController.listProducts);
-router.get("/newEmployee", verifyToken, AdminController.newEmployee);
-router.post("/addEmployee", verifyToken, AdminController.addEmployee);
-router.get("/newProduct", verifyToken, AdminController.newProduct);
-router.post("/addProduct", verifyToken, AdminController.addProduct);
-router.get("/customers", verifyToken, AdminController.manageCustomers);
+
+router.get("/products",  AdminController.listProducts);
+router.get("/newEmployee",  AdminController.newEmployee);
+router.post("/addEmployee",  AdminController.addEmployee);
+router.get("/newProduct",  AdminController.newProduct);
+router.post("/addProduct",  AdminController.addProduct);
+router.get("/customers",  AdminController.manageCustomers);
 router.get("/hoadon", AdminController.manageBills);
-router.get("/", verifyToken, AdminController.dashboard);
+router.get("/",  AdminController.dashboard);
 
 module.exports = router;
