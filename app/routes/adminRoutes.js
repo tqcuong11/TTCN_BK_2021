dd2d310ebe03d733cb85590736cab0c45a088a57
mquan247 <quannngo247@gmail.com>
2022-02-05 20:32:45 +0700

    admin

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -4,14 +4,13 @@ const AdminController = require("../controllers/AdminController");
 const auth = require("../controllers/Auth");
 const verifyToken = require("../middlewares/verifyToken");
 
-
-router.get("/products",  AdminController.listProducts);
-router.get("/newEmployee",  AdminController.newEmployee);
-router.post("/addEmployee",  AdminController.addEmployee);
-router.get("/newProduct",  AdminController.newProduct);
-router.post("/addProduct",  AdminController.addProduct);
-router.get("/customers",  AdminController.manageCustomers);
+router.get("/manage-products", AdminController.listProducts);
+router.get("/manage-employees", AdminController.newEmployee);
+router.post("/addEmployee", AdminController.addEmployee);
+router.get("/new-product", AdminController.newProduct);
+router.post("/addProduct", AdminController.addProduct);
+router.get("/customers", AdminController.manageCustomers);
 router.get("/hoadon", AdminController.manageBills);
-router.get("/",  AdminController.dashboard);
+router.get("/", AdminController.dashboard);
 
 module.exports = router;
